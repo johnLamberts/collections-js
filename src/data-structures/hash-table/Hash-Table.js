bb1151a1ba89c1e01b089d83e8ae9a1d8824debc
johnLamberts <faithsacredoo3@gmail.com>
2024-03-13 22:43:32 +0800

    feat: add algorithm

--- src/data-structures/hash-table/Hash-Table.js
+++ src/data-structures/hash-table/Hash-Table.js
@@ -7,4 +7,25 @@ class HashTable {
 
     this.keys = {};
   }
+
+  hash(key) {
+    const hash = Array.from(key).reduce(
+      (hashAcc, keySymbol) => hashAcc + keySymbol.charCodeAt(0),
+      0
+    );
+
+    return hash % this.buckets.length;
+  }
+
+  set(key, value) {
+    const keyHash = this.hash(key);
+
+    this.keys[key] = keyHash;
+
+    const bucketLinkedList = this.buckets[keyHash];
+  }
+
+  has(key) {
+    return Object.hasOwnProperty.call(this.keys, key);
+  }
 }
