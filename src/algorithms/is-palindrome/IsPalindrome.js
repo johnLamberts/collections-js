f92122852efaebc37d17eca22acce1f86777500a
johnLamberts <faithsacredoo3@gmail.com>
2024-03-16 22:00:43 +0800

    feat: is-palindrome algorith add

---
+++ src/algorithms/is-palindrome/IsPalindrome.js
@@ -0,0 +1,13 @@
+const isPalindrome = (s) => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+
+    left += 1;
+    right -= 1;
+  }
+
+  return true;
+};
